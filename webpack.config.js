57f9f87ef687e37e922eced507542160f214923c
Devon Ulrich <dpulrich@gmail.com>
2019-08-05 20:37:40 -0400

    Moved all client-side code to a 'client' folder
    To prepare for server-side code in src/server

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: './src/client/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
@@ -26,7 +26,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./src/index.html"
+            template: "./src/client/index.html"
         })
     ]
 };
