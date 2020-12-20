7c7acc154c7280766f069089de08ecf1752bf202
Devon Ulrich <dpulrich@gmail.com>
2020-12-19 20:57:48 -0500

    Cleaned up server code

--- src/server/app.js
+++ src/server/app.js
@@ -6,25 +6,22 @@ const webpack = require('webpack');
 const webpackConfig = require('../../webpack.config.js');
 const middleware = require('webpack-dev-middleware');
 
+// webserver setup
 let compiler = webpack(webpackConfig);
 app.use(middleware(compiler));
 let server = app.listen(3000, () => console.log("running"));
 
+// game controller setup
 let io = socketio(server);
 
-//create new data structure?
 let gamedata = new Map();
-
 let host = undefined;
 
-//TODO: split up into separate functions
-io.on('connection', function(socket) {
+io.on('connection', (socket) => {
     console.log("connected: " + socket.id + ", host is " + host);
 
     let client = new Client(socket.id);
     gamedata.set(socket.id, client);
-
-    //console.log(JSON.stringify(client));
 
     if(host == undefined) {
         host = socket.id;
@@ -39,7 +36,7 @@ io.on('connection', function(socket) {
         }
     }
 
-    socket.on('disconnect', function() {
+    socket.on('disconnect', () => {
         console.log("disconnected: " + socket.id);
         gamedata.delete(socket.id);
 
@@ -57,7 +54,7 @@ io.on('connection', function(socket) {
         if(client.isPlaying) socket.broadcast.emit("LEAVE", socket.id);
     });
 
-    socket.on("NAME", function(name) {
+    socket.on("NAME", (name) => {
         //properties of client change in the map too
         client.name = name;
         client.isPlaying = true;
@@ -67,12 +64,12 @@ io.on('connection', function(socket) {
         console.log(name + " joined");
     });
 
-    socket.on("HOST_START", function() {
+    socket.on("HOST_START", () => {
         io.to("playingRoom").emit("START", Math.random());
         console.log("game started");
     });
 
-    socket.on("CL_UPDATE", function(data) {
+    socket.on("CL_UPDATE", (data) => {
         client.data = data;
         socket.broadcast.emit("UPDATE", client);
     });
